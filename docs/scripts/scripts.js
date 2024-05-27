59e533eafc6ddfadf856dd6cd9d2ecc6d3e2dbb7
gelthemu <gelthemu@gmail.com>
2024-05-28 02:57:49 +0300

    First commit - add all files to a repository

---
+++ docs/scripts/scripts.js
@@ -0,0 +1,64 @@
+// smooth-scroll-navigation
+document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// nav-pop-up
+document.addEventListener("DOMContentLoaded", () => {
+  const navButton = document.getElementById("nav-button");
+  const navPopup = document.getElementById("nav-popup");
+
+  navButton.addEventListener("click", () => {
+    if (navPopup.style.display === "none" || navPopup.style.display === "") {
+      navPopup.style.display = "flex";
+    } else {
+      navPopup.style.display = "none";
+    }
+  });
+
+  const navPopupItems = document.querySelectorAll(".nav-popup-item");
+  navPopupItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navPopup.style.display = "none";
+    });
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!navButton.contains(event.target) && !navPopup.contains(event.target)) {
+      navPopup.style.display = "none";
+    }
+  });
+});
+
+// email-pop-up-message
+document.addEventListener("DOMContentLoaded", function () {
+  const emailForm = document.getElementById("emailForm");
+  const contactPopup = document.getElementById("contact-popup");
+  const contactClosePopup = document.getElementById("contact-closePopup");
+
+  if (emailForm && contactPopup && contactClosePopup) {
+    emailForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const email = document.getElementById("email").value;
+      const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+      if (isValid) {
+        contactPopup.style.display = "flex";
+      } else {
+        alert("Please enter a valid email address.");
+      }
+      emailForm.reset();
+    });
+
+    contactClosePopup.addEventListener("click", () => {
+      contactPopup.style.display = "none";
+    });
+  } else {
+    console.error("Form, popup, or close button not found.");
+  }
+});
